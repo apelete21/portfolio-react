ede1e173777f8781a76a72fac7abeb99a87ef60c
CoderZ <adzohonouapelete@gmail.com>
2023-01-23 10:31:23 +0000

    components files init

--- src/index.js
+++ src/index.js
@@ -10,6 +10,10 @@ import Contact from './pages/Contact';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
+    <Home/>
+    <Skills/>
+    <Portfolio/>
+    <About/>
+    <Contact/>
   </React.StrictMode>
 );
