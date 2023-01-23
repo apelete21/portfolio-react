2547e7ab77e86ff8a54f199e06626bc1e0a27ae9
CoderZ <adzohonouapelete@gmail.com>
2023-01-23 14:44:41 +0000

    Home developpement

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import Z from '../medias/icons/z.svg'
+import In from '../medias/icons/jam_linkedin-circle.svg'
+import Git from '../medias/icons/jam_github-circle.svg'
+import Tw from '../medias/icons/typcn_social-twitter-circular.svg'
 
 const TopBar = () => {
-  return (
-    <div>TopBar</div>
-  )
+    return (
+        <>
+            <div className='vw-100'>
+                <div>
+                    <img src={Z} alt="logo" />
+                </div>
+                <div>
+                    <ul>
+                        <li>
+                            <a href="">
+                                <img src={In} alt="Linkedin" />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <img src={Git} alt="Github" />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <img src={Tw} alt="Twitter" />
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default TopBar
