40bdaffcfcb0585215ae2a9efcbd747d2aeca939
CoderZ <adzohonouapelete@gmail.com>
2023-01-25 10:01:44 +0000

    About finished

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Me from '../medias/pictures/me.png'
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <div class="about_section justify-between">
+        <div class="img_about">
+          <img src={Me} alt="" srcset="" className='w-100 h-100' />
+        </div>
+        <div class="txt_about">
+          <p class="p1_about">
+            I'm a front-end developer, specialized in react with consequent experiences
+            in web design and development. I love the connection between design and technology, which is reflected in my
+            design approach and vision.
+          </p><br />
+          <p class="p1_about">
+            I'm very strict and fastidious and on my free time I love
+            playing soccer with some friends, Playstation games, and even watch instructive video series, and my favorite
+            is to increase my professional skills.
+          </p><br />
+          <Link class="btn_about mr-10" to="/contact"><b>Let's start</b></Link>
+          <Link class="btn_about" to={'/portfolio'}><b>View my recent works</b></Link>
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import Tel from '../medias/icons/tel.svg'
+import Map from '../medias/icons/map.svg'
+import Mail from '../medias/icons/mail.svg'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className="contact w-100 h-100 dflex justify-between">
+      <div className="address">
+        <div className="addressIcon">
+          <div className="test">
+            <div className="text">You have a project ? let's <br /> discuss on it</div>
+            <div>
+              <div className="icon">
+                <img src={Tel} alt="" />
+                <span>(00228) 93 91 75 91 / 98 05 17 45</span>
+              </div>
+              <div className="icon">
+                <img src={Map} alt="" /><span> Domicile Adzohonou Kodjo
+                  Semanou,
+                  Golfe <br /> Lomé-TOGO</span>
+              </div>
+              <div className="icon">
+                <img src={Mail}
+                  alt="" />
+                <span>adzohonouapelete@gmail.com</span>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+      <div className="contactForm">
+        <h2 className="title">Contact Me</h2>
+        <form action="">
+          <input type="text" placeholder="Name" />
+          <input type="mail" placeholder="Email" />
+          <textarea name="" id="" placeholder="Message here...">
+
+          </textarea>
+          <button className="btn">Send</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
