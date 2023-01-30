9e1124c1c51d69d8936d0a3ea5bdc06e572fe8fe
CoderZ <adzohonouapelete@gmail.com>
2023-01-30 21:55:37 +0000

    menu socials icons added done

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,6 +3,9 @@ import { Link, useLocation } from 'react-router-dom'
 import { AppContext } from '../contexts/Context'
 import Animation from './Animation'
 import MenuClose from '../medias/icons/close-menu.svg'
+import In from '../medias/icons/jam_linkedin-circle.svg'
+import Git from '../medias/icons/jam_github-circle.svg'
+import Tw from '../medias/icons/typcn_social-twitter-circular.svg'
 
 const Menu = () => {
 
@@ -25,7 +28,21 @@ const Menu = () => {
           <li><Link onClick={() => ToggleMenuState()} to={'/portfolio'} className={location.pathname === '/portfolio' ? 'm_active' : ''}>Portfolio</Link></li>
           <li><Link onClick={() => ToggleMenuState()} to={'/about'} className={location.pathname === '/about' ? 'm_active' : ''}>About</Link></li>
           <li><Link onClick={() => ToggleMenuState()} to={'/contact'} className={location.pathname === '/contact' ? 'm_active' : ''}>Contact</Link></li>
+          
+            <div className="social-menu dflex justify-center">
+              <Link className="social_icon" target={'_blank'} to="http://">
+                <img src={In} alt='Linkedin' />
+              </Link>
+              <Link className="social_icon" target={'_blank'} to="http://">
+                <img src={Git} alt='Github' />
+              </Link>
+              <Link className="social_icon" target={'_blank'} to="http://">
+                <img src={Tw} alt='Twitter' />
+              </Link>
+            </div>
+            
         </ul>
+
         <div className="menu-closer" onClick={() => ToggleMenuState()}>
           <img src={menuState !== true ? Menu : MenuClose} alt='Menu openner' className='w-100' />
         </div>
